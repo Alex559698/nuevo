6e426fc61ecd4a7d28e492fb803948af483a99b6
Tu Nombre <ctoapanta763@gmail.com>
2025-05-25 16:01:50 -0500

    Guarda cambios antes de cambiar de rama

--- FormularioApp/routes/api.php
+++ FormularioApp/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonaController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/persona/{id}', [PersonaController::class, 'show']);
+Route::get('/persona/{id}', [PersonaController::class, 'show']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
