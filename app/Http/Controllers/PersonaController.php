4852203524696920201da748a73f0a108a888651
Alex559698 <ctoapanta763@gmail.com>
2025-05-25 17:20:15 -0500

    Create PersonaController.php
    Crear PersonaController con método mostrar

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller
+{
+public function show($id)
+{
+    $persona = Persona::find($id);
+
+    if (!$persona) {
+        return response()->json(['mensaje' => 'Persona no encontrada'], 404);
+    }
+
+    return response()->json($persona);
+}
